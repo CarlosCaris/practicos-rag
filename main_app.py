db228a4f660353b024b7eff9479a56f3c018f5aa
JoseCarlosCaris <josecarlos@thenotcompany.com>
2024-11-19 09:33:48 -0300

    example of a import for a local module

--- main_app.py
+++ main_app.py
@@ -1,3 +1,9 @@
+"""Este script contiene la aplicación principal."""
+
 import streamlit as st
+from src.chunking.chunk import chunk_document
 
 st.title("Hello")
+
+
+chunk_document(Document(page_content="Hola, ¿cómo estás?"))
